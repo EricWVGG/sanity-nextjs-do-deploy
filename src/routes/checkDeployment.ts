be58fcf6cf9967ef2b8498661651d9d4e8893385
Eric Jacobsen <eric@whiskyvangoghgo.com>
2025-10-20 21:13:53 -0400

    another nextjs try

--- src/routes/checkDeployment.ts
+++ src/routes/checkDeployment.ts
@@ -1,35 +1,37 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export const checkDeployment = (token?: string, appId?: string) => async (request: NextRequest, context: { params: Promise<{}> }) => {
-  if (!token || !appId) {
-    throw new Error("missing required token and appId")
-  }
-
-  try {
-    const headers = {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
+export const checkDeployment =
+  (token?: string, appId?: string) =>
+  async (request: NextRequest, context: { params: Promise<{}> }): Promise<Response> => {
+    if (!token || !appId) {
+      throw new Error("missing required token and appId")
     }
 
-    const parameters = request.nextUrl.searchParams
-    const deploymentId = parameters.get("id")
-    if (!deploymentId) {
-      const response = await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments?page=1&per_page=1`, {
-        method: "GET",
-        headers,
-      })
-      const data = await response.json()
-      return NextResponse.json(data)
-    } else {
-      const response = await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments/${deploymentId}`, {
-        method: "GET",
-        headers,
-      })
-      const data = await response.json()
-      return NextResponse.json(data)
+    try {
+      const headers = {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      }
+
+      const parameters = request.nextUrl.searchParams
+      const deploymentId = parameters.get("id")
+      if (!deploymentId) {
+        const response = await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments?page=1&per_page=1`, {
+          method: "GET",
+          headers,
+        })
+        const data = await response.json()
+        return NextResponse.json(data)
+      } else {
+        const response = await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments/${deploymentId}`, {
+          method: "GET",
+          headers,
+        })
+        const data = await response.json()
+        return NextResponse.json(data)
+      }
+    } catch (error) {
+      console.log(error)
+      return NextResponse.json({ message: error }, { status: 400 })
     }
-  } catch (error) {
-    console.log(error)
-    return NextResponse.json({ message: error }, { status: 400 })
   }
-}
