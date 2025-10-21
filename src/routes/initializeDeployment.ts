e8f83368a081ec80f30dbcb0a0b6da0815a5c74a
Eric Jacobsen <eric@whiskyvangoghgo.com>
2025-10-20 21:01:51 -0400

    NextJS is being fussy about typing routes

--- src/routes/initializeDeployment.ts
+++ src/routes/initializeDeployment.ts
@@ -1,28 +1,30 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export const initializeDeployment = (token?: string, appId?: string) => async () => {
-  if (!token || !appId) {
-    throw new Error("missing required token and appId")
-  }
-
-  try {
-    const headers = {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
+export const initializeDeployment =
+  (token?: string, appId?: string) =>
+  async (_: NextRequest, __: { params: Promise<{}> }): Promise<Response> => {
+    if (!token || !appId) {
+      throw new Error("missing required token and appId")
     }
 
-    const body = JSON.stringify({
-      force_build: true,
-    })
+    try {
+      const headers = {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      }
 
-    const response = await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments`, {
-      method: "POST",
-      body,
-      headers,
-    })
+      const body = JSON.stringify({
+        force_build: true,
+      })
 
-    return NextResponse.json({ ...response, status: 200 })
-  } catch (error) {
-    return NextResponse.json({ message: error }, { status: 402 })
+      const response = await fetch(`https://api.digitalocean.com/v2/apps/${appId}/deployments`, {
+        method: "POST",
+        body,
+        headers,
+      })
+
+      return NextResponse.json({ ...response, status: 200 })
+    } catch (error) {
+      return NextResponse.json({ message: error }, { status: 402 })
+    }
   }
-}
